2d99b4b6938b18b7ceba9969964d6db66ee92763
Cole Michaels <cmichaelsd@gmail.com>
2021-08-16 08:57:43 +0900

    Completed Mult.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,52 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    // Initalize R2 as 0
+    @R2
+    M=0
+
+    // If R0 or R1 is 0 end
+    @R0
+    D=M
+    @END
+    D;JEQ
+    @R1
+    D=M
+    @END
+    D;JEQ
+
+    // Save R0 as count
+    @R0
+    D=M
+    @count
+    M=D
+
+    // Save R1 as R2
+    @R1
+    D=M
+    @R2
+    M=D
+
+  (LOOP)
+    // Minus count
+    @count
+    M=M-1
+    D=M
+
+    // If count is 0 end
+    @END
+    D;JEQ
+
+    // Add R1 to R2
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    // Loop
+    @LOOP
+    0;JMP
+
+  (END)
+    @END
+    0;JMP
