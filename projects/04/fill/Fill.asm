3a91149c1b98ce05945a4fc6fa98379e997b7c15
Cole Michaels <cmichaelsd@gmail.com>
2021-08-26 14:58:39 +0900

    Completed Fill.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,50 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  (BEGIN)
+    // Save SCREEN address
+    @SCREEN
+    D=A
+    // Save SCREEN address to pointer
+    @pointer
+    M=D
+  (LOOP)
+    // Save keyboard input
+    @KBD
+    D=M
+    @WHITE
+    D;JEQ
+    @BLACK
+    0;JMP
+  (WHITE)
+    // Direct to current SCREEN address
+    @pointer
+    A=M
+    // Save current SCREEN address as 0
+    M=0
+    @INC
+    0;JMP
+  (BLACK)
+    // Direct to current SCREEN address
+    @pointer
+    A=M
+    // Save current SCREEN address as 0
+    M=-1
+    @INC
+    0;JMP
+  (INC)
+    // Save current SCREEN address + 1
+    @pointer
+    D=M+1
+    // Save current SCREEN address + 1 to pointer
+    M=D
+    // KBD's address = 24576
+    // SCREENS initial address = 16384 (at this point +1)
+    // KBD address - current SCREEN address (iterates 8191 times)
+    @KBD
+    D=A-D
+    @BEGIN
+    D;JEQ
+    @LOOP
+    0;JMP
+    
